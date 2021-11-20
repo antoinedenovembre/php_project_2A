c745db764ac18d8cc97e3ac322373224f1ee6a8d
antoine <aduteyrat@gmail.com>
2021-11-20 17:31:57 +0100

    Fin du NewsGateway.php Ajout de la méthode ValidBool dans Validation.php

--- Controller/DAL/NewsGateway.php
+++ Controller/DAL/NewsGateway.php
@@ -15,6 +15,14 @@ class NewsGateway
         $this->con = $con;
     }
 
+    /**
+     * @param string $site
+     * @param string $titre
+     * @param string $dateGet
+     * @param string $lien
+     * @param bool $isfrench
+     * @return int
+     */
     public function insert(string $site, string $titre, string $dateGet, string $lien, bool $isfrench) : int
     {
         $query = 'INSERT INTO News(site, titre, dateGet, lien, isfrench) VALUES(:site, :titre, :dateGet, :lien, :isfrench)';
@@ -32,12 +40,26 @@ class NewsGateway
             ':isfrench' => array($isfrench, PDO::PARAM_BOOL)
         );
 
-        $this->con->executeQuery($query, $params);
-
-        return $this->con->lastInsertId();
+        if ($site<>"" || $titre<>0 || $dateGet<>"" || $lien<>"") {
+            $this->con->executeQuery($query, $params);
+            return $this->con->lastInsertId();
+        }
+        return 0;
+        /*
+         * en cas de mauvaises informations, pas d'insertion et renvoie de 0 (id null)
+         * pour signifier la mauvaise insertion
+         */
     }
 
-    public function update(int $id, string $site, string $titre, string $dateGet, string $lien, bool $isfrench)
+    /**
+     * @param int $id
+     * @param string $site
+     * @param string $titre
+     * @param string $dateGet
+     * @param string $lien
+     * @param bool $isfrench
+     */
+    public function update(int $id, string $site, string $titre, string $dateGet, string $lien, bool $isfrench) : void
     {
         $query =    'UPDATE News 
                     SET site = :site, titre = :titre, dateGet = :dateGet, lien = :lien, isfrench = :isfrench
@@ -47,6 +69,7 @@ class NewsGateway
         Validation::validString($titre);
         Validation::validString($dateGet);
         Validation::validString($lien);
+        Validation::validInt($id);
 
         $params = array(
             ':id' => array($id, PDO::PARAM_INT),
@@ -57,18 +80,52 @@ class NewsGateway
             ':isfrench' => array($isfrench, PDO::PARAM_BOOL)
         );
 
-        $this->con->executeQuery($query, $params);
+        if ($id<>0 || $site<>"" || $titre<>0 || $dateGet<>"" || $lien<>"") {
+            $this->con->executeQuery($query, $params);
+        }
     }
 
-    public function delete(int $id)
+    /**
+     * @param int $id
+     */
+    public function delete(int $id) : void
     {
-        // pareil qu'au dessus en delete
+        $query = 'DELETE FROM News WHERE id = :id';
+
+        Validation::validInt($id);
+
+        $params = array(
+            ':id' => array($id, PDO::PARAM_INT)
+        );
+
+        if ($id<>0) {
+            $this->con->executeQuery($query, $params);
+        }
     }
 
-    public function FindByDate(str $date) : array
+    /**
+     * @param string $date
+     * @return array
+     */
+    public function FindByDate(string $date) : array
     {
-        // recup données côté db (preparation + exec)
-        // conversion en objets
+        // recup données côté db (preparation + exec grâce a Connection::executeQuery())
+        Validation::validString($date);
+        if ($date<>"") {
+            $query = 'SELECT * FROM News WHERE dateGet = :dateGet';
+            $params = array(
+                ':dateGet' => array($date, PDO::PARAM_STR)
+            );
+
+            $this->con->executeQuery($query, $params);
+
+            // conversion en objets
+            $res = $this->con->getResults();
+            $tabN = array();
+            foreach ($res as $row)
+                $tabN[] = new News($row['id'], $row['site'], $row['titre'], $row['dateGet'], $row['lien'], $row['isfrench']);
+            return $tabN;
+        }
         return array();
     }
 }
--- Controller/Validation.php
+++ Controller/Validation.php
@@ -3,40 +3,47 @@
 class Validation
 {
     /**
-     * @param $str
+     * @param string $str
      */
     public static function validString(string & $str) : void
     {
-        if (!isset($str) || empty($str = filter_var($str, FILTER_SANITIZE_STRING)))
-        {
-            $str = "stringError";
+        if (!isset($str) || empty($str = filter_var($str, FILTER_SANITIZE_STRING))) {
+            $str = "";
         }
     }
 
     /**
-     * @param $mail
+     * @param string $mail
      */
     public static function validMail(string & $mail) : void
     {
         if (!isset($mail) || empty($mail = filter_var($mail, FILTER_SANITIZE_EMAIL)) ||
-            !filter_var($mail, FILTER_VALIDATE_EMAIL))
-        {
-            $mail = "mail@example.com";
+            !filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+            $mail = "";
         }
     }
 
     /**
-     * @param $int
+     * @param string $int
      */
     public static function validInt(string & $int) : void
     {
-        if (!isset($int) || empty($int = filter_var($int, FILTER_SANITIZE_NUMBER_INT)))
-        {
+        if (!isset($int) || empty($int = filter_var($int, FILTER_SANITIZE_NUMBER_INT))) {
             $int = 0;
-        }
-        else
-        {
+        } else {
             $int = filter_var($int, FILTER_VALIDATE_INT);
+        }
+    }
+
+    /**
+     * @param bool $b
+     */
+    public static function validBool(bool & $b) : void
+    {
+        if (!isset($b)) {
+            $b = false;
+        } else {
+            $b = filter_var($b, FILTER_VALIDATE_BOOL);
         }
     }
 }
